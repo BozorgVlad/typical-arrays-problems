36e74a7f049e15105ee238440a727bbaf714c8c1
BozorgVlad <bozorgvlad@gmail.com>
2020-03-01 17:09:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    array.sort((a,b) => {
+      return a -b;
+    });
+    return array[0];
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    array.sort((a,b) => {
+      return b - a;
+    });
+    return array[0];
+  } else {
   return 0;
+}
 }
 
 exports.avg = function avg (array) {
+  if ( Array.isArray(array) && array.length > 0) {
+  let res = 0;
+  for (let i = 0; i < array.length; i++) {
+    res += array[i];
+  }
+  return res / array.length;
+} else {
   return 0;
+}
 }
